03cda0906fa159558c4e42e2b0a17d7868259ff1
Brian Schiller <bgschiller@gmail.com>
2017-06-28 08:17:54 -0600

    comments through channels

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule Pxblog.CommentControllerTest do
   use Pxblog.ConnCase
 
+  alias Pxblog.Comment
+
   import Pxblog.Factory
 
   @valid_attrs %{author: "Some Person", body: "This is a sample comment"}
@@ -9,15 +11,19 @@ defmodule Pxblog.CommentControllerTest do
   setup do
     user = insert(:user)
     post = insert(:post, user: user)
+    comment = insert(:comment)
 
-    {:ok, conn: build_conn(), user: user, post: post}
+    {:ok, conn: build_conn(), user: user, post: post, comment: comment}
+  end
+
+  defp login_user(conn, user) do
+    post conn, session_path(conn, :create), user: %{username: user.username, password: user.password}
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn, post: post} do
     conn = post conn, post_comment_path(conn, :create, post), comment: @valid_attrs
     assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
-    conn = get conn, user_post_path(conn, :show, post.user, post)
-    assert html_response(conn, 200) =~ @valid_attrs.body
+    get conn, user_post_path(conn, :show, post.user, post)
     assert Repo.get_by(assoc(post, :comments), @valid_attrs)
   end
 
@@ -26,4 +32,28 @@ defmodule Pxblog.CommentControllerTest do
     assert html_response(conn, 200) =~ "Oops, something went wrong"
   end
 
+  test "deletes the comment when logged in as an authorized user", %{conn: conn, post: post, comment: comment, user: user} do
+    conn = login_user(conn, user) |>
+     delete(post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
+    refute Repo.get(Comment, comment.id)
+  end
+
+  test "does not delete the comment when not logged in as an authorized user", %{conn: conn, post: post, comment: comment} do
+    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == page_path(conn, :index)
+    assert Repo.get(Comment, comment.id)
+  end
+
+  test "updates chosen resource and redirects when data is valid and logged in as the author", %{conn: conn, user: user, post: post, comment: comment} do
+    conn = login_user(conn, user) |> put(post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
+    assert redirected_to(conn) == user_post_path(conn, :show, user, post)
+    assert Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
+
+  test "does not update the comment when not logged in as an authorized user", %{conn: conn, post: post, comment: comment} do
+    conn = put(conn, post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
+    assert redirected_to(conn) == page_path(conn, :index)
+    refute Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,8 +4,10 @@
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
 import {Socket} from "phoenix"
+import $ from 'jquery'
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+const userToken = $("meta[name='channel_token']").attr("content")
+let socket = new Socket("/socket", {params: {token: userToken}})
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -51,12 +53,108 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 // Finally, pass the token on connect as below. Or remove it
 // from connect if you don't care about authentication.
 
+
+const CREATED_COMMENT = "CREATED_COMMENT",
+      APPROVED_COMMENT = "APPROVED_COMMENT",
+      DELETED_COMMENT = "DELETED_COMMENT"
+
 socket.connect()
 
+const postId = $('#post-id').val()
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel(`comments:${postId}`, {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+const createComment = (payload) => `
+  <div id="comment-${payload.commentId}" class="comment" data-comment-id="${payload.commentId}">
+    <div class="row">
+      <div class="col-xs-4">
+        <strong class="comment-author">${payload.author}</strong>
+      </div>
+      <div class="col-xs-4">
+        <em>${payload.insertedAt}</em>
+      </div>
+      <div class="col-xs-4 text-right">
+        ${ userToken ? '<button class="btn btn-xs btn-primary approve">Approve</button> <button class="btn btn-xs btn-danger delete">Delete</button>': ''}
+      </div>
+      <div class="row">
+        <div class="Col-xs-12 comment-body">
+          ${payload.body}
+        </div>
+      </div>
+    </div>
+  `
+
+  // REQ 3: Provide the comment's author from the form
+  const getCommentAuthor   = () => $("#comment_author").val()
+  // REQ 4: Provide the comment's body from the form
+  const getCommentBody     = () => $("#comment_body").val()
+  // REQ 5: Based on something being clicked, find the parent comment id
+  const getTargetCommentId = (target) => $(target).parents(".comment").data("comment-id")
+  // REQ 6: Reset the input fields to blank
+  const resetFields = () => {
+    $("#comment_author").val("")
+    $("#comment_body").val("")
+  }
+
+$('.create-comment').on('click', (event) => {
+  event.preventDefault()
+  channel.push(CREATED_COMMENT, {
+    author: getCommentAuthor(),
+    body: getCommentBody(),
+    postId
+  })
+  resetFields()
+})
+
+
+// REQ 8: Push the APPROVED_COMMENT event to the socket with the appropriate author/body/comment id
+$(".comments").on("click", ".approve", (event) => {
+  event.preventDefault()
+  const commentId = getTargetCommentId(event.currentTarget)
+  // Pull the approved comment author
+  const author = $(`#comment-${commentId} .comment-author`).text().trim()
+  // Pull the approved comment body
+  const body = $(`#comment-${commentId} .comment-body`).text().trim()
+  channel.push(APPROVED_COMMENT, { author, body, commentId, postId })
+})
+
+
+// REQ 9: Push the DELETED_COMMENT event to the socket but only pass the comment id (that's all we need)
+$(".comments").on("click", ".delete", (event) => {
+  event.preventDefault()
+  const commentId = getTargetCommentId(event.currentTarget)
+  channel.push(DELETED_COMMENT, { commentId, postId })
+})
+
+// REQ 10: Handle receiving the CREATED_COMMENT event
+channel.on(CREATED_COMMENT, (payload) => {
+  // Don't append the comment if it hasn't been approved
+  if (!userToken && !payload.approved) { return; }
+  // Add it to the DOM using our handy template function
+  $(".comments h2").after(
+    createComment(payload)
+  )
+})
+
+// REQ 11: Handle receiving the APPROVED_COMMENT event
+channel.on(APPROVED_COMMENT, (payload) => {
+  // If we don't already have the right comment, then add it to the DOM
+  if ($(`#comment-${payload.commentId}`).length === 0) {
+    $(".comments h2").after(
+      createComment(payload)
+    )
+  }
+  // And then remove the "Approve" button since we know it has been approved
+  $(`#comment-${payload.commentId} .approve`).remove()
+})
+
+// REQ 12: Handle receiving the DELETED_COMMENT event
+channel.on(DELETED_COMMENT, (payload) => {
+  // Just delete the comment from the DOM
+  $(`#comment-${payload.commentId}`).remove()
+})
 
 export default socket
